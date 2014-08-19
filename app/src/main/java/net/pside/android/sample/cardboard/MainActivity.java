e470b58f207aa5a621c9b584eedf87cd3c7ef608
IMANAKA, Kouta <imanaka@seesaa.co.jp>
2014-08-19 20:10:38 +0900

    三角形をグルグル回す

--- app/src/main/java/net/pside/android/sample/cardboard/MainActivity.java
+++ app/src/main/java/net/pside/android/sample/cardboard/MainActivity.java
@@ -9,11 +9,28 @@ import com.google.vrtoolkit.cardboard.EyeTransform;
 import com.google.vrtoolkit.cardboard.HeadTransform;
 import com.google.vrtoolkit.cardboard.Viewport;
 
+import java.io.IOException;
+
 import javax.microedition.khronos.egl.EGLConfig;
+
+import jp.nyatla.nymmd.MmdException;
+import jp.nyatla.nymmd.android.AndMmdMotionPlayerGLES20;
+import jp.nyatla.nymmd.android.AndMmdPmdModel;
+import jp.nyatla.nymmd.android.AndMmdVmdMotion;
 
 public class MainActivity extends CardboardActivity
         implements CardboardView.StereoRenderer {
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    // PMD model / VMD motion
+    AndMmdPmdModel mPmdModel;
+    AndMmdVmdMotion mVmdMotion;
+
+    AndMmdMotionPlayerGLES20 mPlayer;
+
+    MyRenderer mRenderer;
+
+    private int mGlProgram;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +41,32 @@ public class MainActivity extends CardboardActivity
         CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
         cardboardView.setRenderer(this);
         setCardboardView(cardboardView);
+
+        mRenderer = new MyRenderer();
+
+        try {
+            mPmdModel = new AndMmdPmdModel(getAssets(), "model/Miku_Hatsune.pmd");
+            mVmdMotion = new AndMmdVmdMotion(getAssets(), "motion/kishimen.vmd");
+        } catch (MmdException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     // region CardboardView.StereoRenderer
     @Override
     public void onNewFrame(HeadTransform headTransform) {
         Log.d(TAG, "onNewFrame()");
+
+        mRenderer.onNewFrame(null);
     }
 
     @Override
     public void onDrawEye(EyeTransform eyeTransform) {
         Log.d(TAG, "onDrawEye()");
+
+        mRenderer.onDrawFrame(null);
+
+//        mPlayer.render(this);
     }
 
     @Override
@@ -43,20 +75,33 @@ public class MainActivity extends CardboardActivity
     }
 
     @Override
-    public void onSurfaceChanged(int i, int i2) {
-        Log.d(TAG, "onSurfaceChanged()");
+    public void onSurfaceChanged(int width, int height) {
+        Log.d(TAG, "onSurfaceChanged(" + width + "," + height + ")");
+
+        mRenderer.onSurfaceChanged(null, width / 2, height / 2);
     }
 
     @Override
     public void onSurfaceCreated(EGLConfig eglConfig) {
         Log.d(TAG, "onSurfaceCreated()");
 
-//        GLES20
+        mRenderer.onSurfaceCreated(null, eglConfig);
+
+/*        mPlayer = new AndMmdMotionPlayerGLES20();
+
+        try {
+            mPlayer.setPmd(mPmdModel);
+            mPlayer.setVmd(mVmdMotion);
+        } catch (MmdException e) {
+            e.printStackTrace();
+        }
+        */
     }
 
     @Override
     public void onRendererShutdown() {
         Log.d(TAG, "onRenderShutdown()");
+//        mPlayer.dispose();
     }
     // endregion
 }
